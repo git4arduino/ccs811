ed90700fce539451771ad905885fdee9a9ea7a86
pixeledi <hello@pixeledi.eu>
2023-08-18 09:27:57 +0200

    added sensorvalues on oled

--- ccs811/ccs811.ino
+++ ccs811/ccs811.ino
@@ -12,15 +12,72 @@ www.pixeledi.eu | https://linktr.ee/pixeledi
 CCS811 Sensor | V1.0 | 10.2023
 */
 
-#include <Arduino.h>
 #include <Wire.h>
 #include "Adafruit_CCS811.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 Adafruit_CCS811 ccs;
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+unsigned long previousMillis = 0;
+uint16_t eco2, etvoc;
+
+void showSensorValues()
+{
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  // eco2 data.
+  display.setCursor(0, 2);
+  display.setTextSize(2);
+  display.print("eco2");
+
+  display.setCursor(10, 35);
+  display.setTextSize(1);
+  display.print(eco2);
+  display.print(" ppm ");
+
+  display.drawLine(SCREEN_WIDTH / 2, 30, SCREEN_WIDTH / 2, SCREEN_HEIGHT, WHITE);
+
+  byte halfscreen = SCREEN_WIDTH / 2;
+  display.setTextSize(2);
+  display.setCursor(halfscreen, 2);
+  display.print("etvoc");
+
+  display.setCursor(halfscreen + 20, 35);
+  display.setTextSize(1);
+  display.print(etvoc);
+  display.print(" ppb ");
+
+  display.display();
+}
+
+void getSensorValues()
+{
+
+if (ccs.available()) {
+    if (!ccs.readData()) {
+      //globals
+      eco2 = ccs.geteCO2();
+      etvoc = ccs.getTVOC();
+    } else {
+      Serial.println("ERROR!");
+      while (1)
+        ;
+    }
+  }
+}
 
 void setup() {
 
   Serial.begin(115200);
+  Wire.begin();
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  delay(100);
 
   if (!ccs.begin()) {
     Serial.println("Failed to start sensor! Please check your wiring.");
@@ -34,17 +91,15 @@ void setup() {
 }
 
 void loop() {
-  if (ccs.available()) {
-    if (!ccs.readData()) {
-      Serial.print("CO2: ");
-      Serial.print(ccs.geteCO2());
-      Serial.print("ppm, TVOC: ");
-      Serial.println(ccs.getTVOC());
-    } else {
-      Serial.println("ERROR!");
-      while (1)
-        ;
-    }
+
+
+unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= 5000)
+  {
+    previousMillis = currentMillis;
+    getSensorValues();
+    showSensorValues();
   }
-  delay(500);
+ 
 }
